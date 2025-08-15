cae2339ad1f6db26d311c1c3a1d6786d67d4f908
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-15 18:59:44 +0530

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -7,8 +7,12 @@ st.set_page_config(layout="wide")
 st.image("charan photo.jpg",)
 
 st.markdown(
-        "<p style='font-size:18px;'>My self Charan Kumar Reddy, Data Engineer by profession skilled in Visualization, Transforming and 
-        managing the data.</p>",
-        unsafe_allow_html=True
-    )
+    """
+    <p style='font-size:18px;'>
+    My self Charan Kumar Reddy, Data Engineer by profession skilled in Visualization,
+    Transforming and managing the data.
+    </p>
+    """,
+    unsafe_allow_html=True
+)
 st.write('want to know here')
