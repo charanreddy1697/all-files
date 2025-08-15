da64b4130a5a7eb5d7c4f15c714199cf1be034cf
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-16 01:28:37 +0530

    csv generator.py

--- csv generator.py
+++ csv generator.py
@@ -15,9 +15,8 @@ amount_max = b2.number_input('Max amount', 10, 1000, 10)
 
 # Column name for field values
 field_col_name = st.text_input('Enter field name (like City or Region)', placeholder='City')
-c1, c2, c3 = st.columns(3)
 st.write('Enter the possible values for the field')
-
+c1, c2, c3 = st.columns(3)
 # Field values
 
 value1 = c1.text_input("", placeholder='Value1')
