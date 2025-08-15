4ed8eda0bee17c7f7f89c6df94213a74477a46f3
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-16 01:46:35 +0530

    csv generator.py

--- csv generator.py
+++ csv generator.py
@@ -45,7 +45,7 @@ if st.button('Generate CSV'):
     st.write('displaying top 5 rows')
 
     v1 = df.groupby('field_col_name').count()
-    st.write(v1)
+    st.dataframe(v1)
     
     # Download
     csv = df.to_csv(index=False)
