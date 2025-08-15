fc33cba21c0013a7c994b5ba780e1444cf3efb1c
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-15 18:57:15 +0530

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title('WELCOME')
 
+st.set_page_config(layout="wide")
+
 st.image("charan photo.jpg",)
 
 st.header('''My Self Charan Kumar Reddy Somagari, Data engineer by profession skilled in Visualization, Tranformation and managing the data,
