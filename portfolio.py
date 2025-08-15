3b93d2b4c77a1908c0e385664201bfa1a09ab896
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-15 18:24:00 +0530

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,4 +3,5 @@ import streamlit as st
 st.title('CHARAN KUMAR REDDY SOMAGARI')
 
 c1,c2,c3 = st.columns([3,1,1])
-st.image("charan photo.jpg", caption="This is me")
+with c2:
+  st.image("charan photo.jpg", caption="This is me")
