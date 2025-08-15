758b4cd26d43b5aea67f87223d9a580342e6cf01
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-16 01:24:38 +0530

    csv generator.py

--- csv generator.py
+++ csv generator.py
@@ -6,25 +6,24 @@ st.title('YOUR CSV GENERATOR')
 
 # Rows input
 
-a1, a2 = st.columns(2)
-a1.write('Enter the number of rows')
-rows = a2.number_input('', min_value=1, max_value=10000, value=5)
-
+st.write('Enter the number of rows')
+rows = st.number_input('', min_value=1, max_value=10000, value=5)
+st.write('Specify the range for amount column')
 # Amount range
-b1, b2, b3 = st.columns(3)
-b1.write('Specify the range for amount column')
-amount_min = b2.number_input('Min amount', 1, 10, 1)
-amount_max = b3.number_input('Max amount', 10, 1000, 10)
+b1, b2 = st.columns(2)
+amount_min = b1.number_input('Min amount', 1, 10, 1)
+amount_max = b2.number_input('Max amount', 10, 1000, 10)
 
 # Column name for field values
 field_col_name = st.text_input('Enter field name (like City or Region)', placeholder='City')
 
 # Field values
 c1, c2, c3 = st.columns(3)
-c1.write('Enter the possible values for the field')
+st.write('Enter the possible values for the field')
 value1 = c1.text_input("", placeholder='Value1')
 value2 = c2.text_input("", placeholder='Value2')
 value3 = c3.text_input("", placeholder='Value3')
+
 
 # Generate CSV Button
 if st.button('Generate CSV'):
