3760503382ee5af45294180d6be73a0e19b85c6a
CHARAN KUMAR REDDY SOMAGARI <charanreddysomagari@gmail.com>
2025-08-17 11:52:51 +0530

    text generator.py

--- text generator.py
+++ text generator.py
@@ -11,8 +11,8 @@ c1,c2 = st.columns([1,1])
 font_size = c1.slider("Font size", 10, 50, 27)
 
 colr = st.color_picker("Pick a background color", "#FFFFFF")
-image_width = st.number_input('Weidth',100,600,900)
-image_height = st.number_input('Height',100,300,600)
+image_width = st.number_input('Weidth',100,900,600)
+image_height = st.number_input('Height',100,600,300)
 try:
         
     # User input
@@ -54,38 +54,3 @@ try:
     st.write('Here your Image')
 except:
     st.write('your have type more than 5 lines')
-# # Button to generate image
-# if a1.button("Generate Image"):
-#     # Create blank white image
-    
-#     img = Image.new("RGB", (600, 450), color=colr)
-#     draw = ImageDraw.Draw(img)
-
-#     font = ImageFont.load_default(size=font_size)
-
-#     # Handle multi-line text
-#     lines_limit = [30,90,150,210,270]
-#     lines = list(user_text.split("\n"))
-#     if len(lines) > 5:
-#         st.warning('only 5 lines allowed')
-#     else:
-            
-        
-#         for i in range(len(lines)):
-#             draw.text((50, lines_limit[i]), lines[i], fill="black", font=font)
-        
-#         # Save to buffer
-#         buf = io.BytesIO()
-#         img.save(buf, format="PNG")
-#         buf.seek(0)
-    
-#         # Display image
-#         st.image(buf, caption="Generated Image", use_container_width=True)
-        
-#         # Download button
-#         a2.download_button(
-#             label="Download Image",
-#             data=buf,
-#             file_name="text_image.png",
-#             mime="image/png"
-#         )
